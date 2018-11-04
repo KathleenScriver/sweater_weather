40b104013f27106b9aaa2796ed19d10860c8172d
Kathleen Yruegas <kathleenecn@gmail.com>
2018-11-03 20:52:42 -0600

    Changed methods to pull out lat and lng'

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -6,7 +6,24 @@ class ForecastFacade
     @id = 1
   end
 
-  def coordinates
-    GeocoderService.new(location).geometry
+  def get_lat
+    geocode_data.geometry[:lat]
+  end
+
+  def get_lng
+    geocode_data.geometry[:lng]
+  end
+
+  def weather
+  # DarkSkyService.new(coordinates)
+  conn = Faraday.new(url: 'https://api.darksky.net')
+  response = conn.get("/forecast/#{ENV['DARK_SKY_API_KEY']}/[latitude],[longitude]")
+
+  end
+
+  private
+
+  def geocode_data
+    @geocode ||= GeocoderService.new(location)
   end
 end
--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -15,14 +15,25 @@ describe ForecastFacade do
   end
 
   context 'instance methods' do
-    context '#coordinates' do
-      it 'should return latitude and longitude' do
+    context '#get_lat' do
+      it 'should return latitude' do
         denver_lat = 39.7392358
-        denver_lng = -104.990251
 
-        expect(subject.coordinates[:lat]).to eq(denver_lat)
-        expect(subject.coordinates[:lng]).to eq(denver_lng)
+        expect(subject.get_lat).to eq(denver_lat)
       end
     end
+
+    context '#get_lng' do
+      it 'should return longitude' do
+        denver_lng = -104.990251
+
+        expect(subject.get_lng).to eq(denver_lng)
+      end
+    end
+
+    context '#weather'
+      it 'should return forecast objects' do
+
+      end
   end
 end

60b9651d36322b1cdf8742941d2924c6bb4e178d
Kathleen Yruegas <kathleenecn@gmail.com>
2018-11-05 15:04:45 -0700

    Adds request spec for background image

---
+++ spec/requests/api/v1/background_request_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+describe "Background API" do
+  it 'should return background image of location' do
+    location = 'denver,co'
+
+    get "/api/v1/background?#{location}"
+
+    image_info = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(image_info).to eq("link")
+  end
+end

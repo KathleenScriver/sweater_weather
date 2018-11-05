4d80d06ba87542c16dbee0cdcd346e708a1c76f3
Kathleen Yruegas <kathleenecn@gmail.com>
2018-11-05 11:41:49 -0700

    Adds daily weather to serializer

--- app/serializers/forecast_facade_serializer.rb
+++ app/serializers/forecast_facade_serializer.rb
@@ -1,5 +1,5 @@
 class ForecastFacadeSerializer < ActiveModel::Serializer
   include FastJsonapi::ObjectSerializer
   set_type :ForecastFacade
-  attributes :location, :current_weather, :hourly_temps
+  attributes :location, :current_weather, :hourly_temps, :daily_weather
 end

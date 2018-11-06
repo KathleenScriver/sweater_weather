b973bd03eee9e5739e714017c1b4210c8317c56a
Kathleen Yruegas <kathleenecn@gmail.com>
2018-11-06 12:40:58 -0700

    Refines background feature test to account for serializer structure

--- spec/requests/api/v1/background_request_spec.rb
+++ spec/requests/api/v1/background_request_spec.rb
@@ -6,8 +6,9 @@ describe "Background API" do
 
     get "/api/v1/background?location=#{location}"
 
-    image_info = JSON.parse(response.body, symbolize_names: true)[:data]
+    image_info = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
 
-    expect(image_info).to eq("link")
+    expect(image_info).to have_key(:location_image)
+    expect(image_info[:location_image]).to include('.jpg')
   end
 end

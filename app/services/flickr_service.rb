9a7f9540d02f8535f9306a7b168af9ed90848224
Kathleen Yruegas <kathleenecn@gmail.com>
2018-11-06 12:37:04 -0700

    Refactors flickr service

--- app/services/flickr_service.rb
+++ app/services/flickr_service.rb
@@ -6,25 +6,37 @@ class FlickrService
   end
 
   def one_image
-    options = {
-               api_key: ENV['FLICKR_API_KEY'],
-               lat: lat,
-               lon: lon,
-               method: "flickr.photos.search",
-               format: "json",
-               nojsoncallback: 1,
-               extras: "url_o",
-               tags: "parks"
-    }
+    all_photos.shuffle.pop[:url_o]
+  end
 
-    conn = Faraday.new(url: 'https://api.flickr.com')
-    response = conn.get do |req|
-      req.url "/services/rest/", options
-    end
-    all_photos = JSON.parse(response.body, symbolize_names: true)[:photos][:photo]
-    photos_with_links = all_photos.select do |photo|
+  def all_photos
+    JSON.parse(response.body, symbolize_names: true)[:photos][:photo].select do |photo|
       photo.keys.include?(:url_o)
     end
-    x = photos_with_links.shuffle.pop[:url_o]
+  end 
+
+  private
+
+  def conn
+    Faraday.new(url: 'https://api.flickr.com')
+  end
+
+  def response
+    conn.get do |req|
+      req.url "/services/rest/", options
+    end
+  end
+
+  def options
+    {
+     api_key: ENV['FLICKR_API_KEY'],
+     lat: lat,
+     lon: lon,
+     method: "flickr.photos.search",
+     format: "json",
+     nojsoncallback: 1,
+     extras: "url_o",
+     tags: "parks"
+    }
   end
 end

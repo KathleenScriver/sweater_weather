c1a27435ef395060126c94bae6839471c3362319
Kathleen Yruegas <kathleenecn@gmail.com>
2018-11-07 11:53:47 -0700

    Adds user save logic to users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,16 @@
 class Api::V1::UsersController < ApplicationController
   def create
+    user = User.new(user_params)
+    if user.save
+     render json: user.api_key
+   else
+     render json: {message: "Error"}, status: 401
+   end
+  end
 
-  end 
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
 end

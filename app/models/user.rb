fcde84b2c5165073e4bb429775e2fea2469c19a2
Kathleen Yruegas <kathleenecn@gmail.com>
2018-11-07 11:58:55 -0700

    Adds api_key column to users table and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
+require 'securerandom'
+
 class User < ApplicationRecord
+  before_save :generate_api_key
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, require: true
   validates_presence_of :password_confirmation, require: true
+  validates_presence_of :api_key, require: true
 
   has_secure_password
+
+  private
+
+  def generate_api_key
+    self.api_key = SecureRandom.uuid
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,6 @@ describe User do
     it { should validate_uniqueness_of(:email) }
     it { should validate_presence_of(:password) }
     it { should validate_presence_of(:password_confirmation) }
+    it { should validate_presence_of(:api_key) }
   end
 end
